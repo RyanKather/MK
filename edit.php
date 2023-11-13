790c8aa877f5cf5f506ea8b5626b4f0eb7e534ad
Ryan Kather <115558506+RyanKather@users.noreply.github.com>
2023-11-12 20:23:31 -0400

    Add files via upload

---
+++ edit.php
@@ -0,0 +1,99 @@
+<?php
+if(!empty($_GET['id']))
+{
+	//print_r($_POST['nome']);
+	//print_r($_POST['email']);
+	//print_r($_POST['senha']);
+
+	include_once('config.php');
+
+    $id = $_GET['id'];
+    
+    $sqlSelect="SELECT * FROM usuario WHERE id=$id";
+
+    $result = $conexao ->query($sqlSelect);
+
+    if($result -> num_rows > 0)
+    {
+        while($user_data = mysqli_fetch_assoc($result))
+        {
+            $nome = $user_data['nome'];
+	        $email = $user_data['email'];
+	        $senha = $user_data['senha'];
+        }
+        //print_r($nome);
+    }
+    else
+    {
+        header('Location: sistema.php');
+    }
+
+}
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Criar conta</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="shortcut icon" type="imagex/png" href="./css/imgs/favicon.ico">
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-6 col-md-6 form-container">
+				<div class="col-lg-8 col-md-12 col-sm-9 col-xs-12 form-box text-center">
+					<div class="logo mt-5 mb-3">
+						<img src="css/img/logo.png" width="150px">
+					</div>
+					<div class="cabeçalho mb-3">
+						<h4>Crie a sua conta aqui</h4>
+					</div>
+					<form action="registro.php" method ="POST">
+						<div class="form-input">
+							<span><i class="fa fa-user"></i></span>
+							<input type="text" name="nome" placeholder="Nome completo" value= "<?php echo $nome ?>" required>
+						</div>
+						<div class="form-input">
+							<span><i class="fa fa-envelope"></i></span>
+							<input type="email" name="email"  placeholder="Endereço de E-mail" value= "<?php echo $email ?>" required>
+						</div>
+						<div class="form-input">
+							<span><i class="fa fa-lock"></i></span>
+							<input type="text" name="senha" placeholder="Senha" value= "<?php echo $senha ?>" required>
+                            
+						</div>
+						<div class="row mb-3">
+							<div class="col-12 d-flex">
+								<div class="custom-control custom-checkbox">
+									<input type="checkbox" class="custom-control-input" id="cb1">
+									<label class="custom-control-label text-white" for="cb1">Concordo com todos os termos e condições</label>
+								</div>
+							</div>
+						</div>
+						<div class="text-left mb-3">
+							<button type="submit" name="submit" href= "sistema.php" class="btn">Registrar</button>
+                           
+						</div>
+						<div class="text-white">já tem uma conta?
+							<a href="login.php" class="login-link">Entre aqui</a>
+						</div>
+					</form>
+                    <input type="hidden" nome="id" value= "<?php echo $id ?>">
+				</div>
+			</div>
+
+			<div class="col-lg-6 col-md-6 d-none d-md-block image-container"></div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ sistema.php
@@ -0,0 +1,93 @@
+<?php
+    session_start();
+    include_once('config.php');
+    if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true))
+    {
+        unset($_SESSION['email']);
+        unset($_SESSION['senha']);
+        header('location: login.php');
+    }
+    $logado = $_SESSION['email'];
+
+    $sql = "SELECT * FROM usuario ORDER BY id DESC";
+
+    $result = $conexao->query($sql);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>SISTEMA | MK</title>
+    <style>
+        body{
+            background-color: rgb(241, 16, 166);
+            color: black;
+            text-align: center;
+        }
+        .table-bg{
+            background: rgba(0,0,0,0.5);
+            border-radius: 15px 15px 0 0
+        }
+
+    </style>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">SISTEMA MK</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <div class="d-flex">
+            <a href="sair.php" class="btn btn-danger me-5">Sair</a>
+        </div>
+    </nav>
+    <br>
+    <?php
+        echo "<h1>Bem vindo <u>$logado</u></h1>";
+    ?>
+    <div class="m-5">
+        <table class="table text-white table-bg">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">nome</th>
+                    <th scope="col">email</th>
+                    <th scope="col">senha</th>
+                    <th scope="col">...</th>
+            </thead>
+            <tbody>
+                
+        </tbody>
+            <?php
+                while($user_data = mysqli_fetch_assoc($result))
+                {
+                    echo "<tr>";
+                    echo "<td>" . $user_data['id'] . "</td>";
+                    echo "<td>" . $user_data['nome'] . "</td>";
+                    echo "<td>" . $user_data['email'] . "</td>";
+                    echo "<td>" . $user_data['senha'] . "</td>";
+                    echo "<td>
+                        <a class='btn btn-sm btn-primary' href='edit.php?id=$user_data[id]'>
+                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil' viewBox='0 0 16 16'>
+                            <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z'/>
+                      </svg>
+                      </a>
+                        <a class='btn btn-sm btn-danger' href='delete.php?id=$user_data[id]'>
+                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash3-fill' viewBox='0 0 16 16'>
+  <path d='M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z'/>
+</svg>
+                        </a>
+                    </td>";
+                    echo "</tr>";
+                }
+            ?>
+        </table>
+    </div>
+</body>
+</html>
